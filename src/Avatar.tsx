3b94a6e8bc1bd0b693c2f78d6de1237c8713c5c4
abdul <malik301002@gmail.com>
2023-01-08 11:24:16 +0700

    feat: create Avatar component

---
+++ src/Avatar.tsx
@@ -0,0 +1,12 @@
+type AvatarProps = {
+  /** the url of an image to render for the Avatar. **/
+  url: string;
+  /** the alt text to use for the url image. **/
+	alt: string;
+};
+
+const Avatar = ({ url, alt }: AvatarProps) => (
+  <img src={url} alt={alt} />
+);
+
+export default Avatar;
