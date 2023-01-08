4690f723795f49f4b75c02e0ba34541c1b23f9bd
abdul <malik301002@gmail.com>
2023-01-08 14:04:30 +0700

    feat: url, alt fallback and onError handler

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+
+export const FALLBACK_AVATAR_URL = 'https://cataas.com/cat/says/fallback';
+export const FALLBACK_AVATAR_ALT_TEXT = '@fallback-image';
+
 type AvatarProps = {
   /** the url of an image to render for the Avatar. **/
-  url: string;
+  url?: string;
   /** the alt text to use for the url image. **/
-	alt: string;
+  alt?: string;
 };
 
-const Avatar = ({ url, alt }: AvatarProps) => (
-  <img src={url} alt={alt} />
-);
+// NOTES
+// if onError is called, use fallback url and alt text
+// 1. useState
+// 2. update src and alt when onError is called
+
+const Avatar = ({
+  url = FALLBACK_AVATAR_URL,
+  alt = FALLBACK_AVATAR_ALT_TEXT,
+}: AvatarProps) => {
+  const [srcToRender, setSrcToRender] = useState<string>(url);
+  return <img src={srcToRender} alt={alt} onError={() => setSrcToRender(FALLBACK_AVATAR_URL)} />;
+};
 
 export default Avatar;
